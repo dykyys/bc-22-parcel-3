c61d11d9095cc78634d12b1b392e5394bc3f0a76
dykyys <56254283+dykyys@users.noreply.github.com>
2022-07-06 09:51:53 +0000

    Deploying to gh-pages from @ dykyys/bc-22-parcel-3@631f956c3b33c6290b688c51e5b658370cbea034 🚀

---
+++ index.2dcc205a.js
@@ -0,0 +1,2 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},o={},n={},r=e.parcelRequired7c6;null==r&&((r=function(e){if(e in o)return o[e].exports;if(e in n){var r=n[e];delete n[e];var l={id:e,exports:{}};return o[e]=l,r.call(l.exports,l,l.exports),l.exports}var i=new Error("Cannot find module '"+e+"'");throw i.code="MODULE_NOT_FOUND",i}).register=function(e,o){n[e]=o},e.parcelRequired7c6=r);const l=(0,r("krGWQ").getRefs)();console.log(l);
+//# sourceMappingURL=index.2dcc205a.js.map
